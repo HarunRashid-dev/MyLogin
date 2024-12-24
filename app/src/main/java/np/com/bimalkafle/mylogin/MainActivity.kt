f6518f233087c158f9f7fc81e1c25210d0c52dad
Admin <harunrashid9159@gmail.com>
2024-12-24 21:36:20 +0530

    This project is done using Easy Tuto Youtube Video. Code is running. Video Link : https://youtu.be/-Kj9T1sa6zk?si=KYAZR13G5i_DJydc

---
+++ app/src/main/java/np/com/bimalkafle/mylogin/MainActivity.kt
@@ -0,0 +1,24 @@
+package np.com.bimalkafle.mylogin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import np.com.bimalkafle.mylogin.ui.theme.MyLoginTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoginScreen()
+        }
+    }
+}
